2d124bda37fee2ae976e7888518d682775b9beee
Fabulous_Rachael <rachael.whitefield@gmail.com>
2020-04-08 15:17:52 -0600

    implemented firebase utils, included ability to store authenticated users in our firestore database

--- src/App.js
+++ src/App.js
@@ -31,10 +31,11 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }, () => {
-            console.log(this.state);
           });
+
+          console.log(this.state);
         });
+        
       } else {
         this.setState({currentUser: userAuth});
       }
--- src/components/sign-up/sign-up.component.jsx
+++ src/components/sign-up/sign-up.component.jsx
@@ -49,7 +49,7 @@ class SignUp extends React.Component {
         }
     };
 
-    handleSubmit = event => {
+    handleChange = event => {
         const { name, value } = event.target;
 
         this.setState({ [name]: value });
